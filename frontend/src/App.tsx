846aa6de4ff8d7808e9c60ba781bfdafea0b81c1
JavierMusso <javier.musso96@gmail.com>
2024-12-29 01:13:47 -0300

    Add initial app layout

---
+++ frontend/src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="">
+      <header className="flex min-h-[100vh] flex-col items-center justify-center bg-[#282c34] text-[29px] text-white">
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+        <a
+          className="text-[#61dafb]"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
