e29526db40177f9e50ee34df52298a1ad3a871ff
JavierMusso <javier.musso96@gmail.com>
2025-07-26 00:58:59 -0300

    Initial display of phrases

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -7,7 +7,9 @@ function App() {
 
   return (
     <div className="flex min-h-[100vh] flex-col items-center justify-center bg-[#282c34] text-[29px] text-white">
-      {phrases.data?.data?.map((p, i) => <div key={i}>{p.content}</div>)}
+      <div className="flex flex-col gap-2">
+        {phrases.data?.data?.map((p, i) => <div key={i}>{p.content}</div>)}
+      </div>
     </div>
   );
 }
