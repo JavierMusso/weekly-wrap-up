8d4884953e992a96a40766a18830f1d38cbc6f71
JavierMusso <javier.musso96@gmail.com>
2025-07-26 03:34:48 -0300

    Make use of new layout contianers

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,5 @@
+import Container from './Components/Layout/Container.tsx';
+import Wrapper from './Components/Layout/Wrapper.tsx';
 import { useGetPhrases } from './hooks/useGetPhrases.ts';
 
 function App() {
@@ -6,11 +8,11 @@ function App() {
   console.log(phrases.data?.data);
 
   return (
-    <div className="flex min-h-[100vh] flex-col items-center justify-center bg-[#282c34] text-[29px] text-white">
-      <div className="flex flex-col gap-2">
+    <Container className="flex min-h-[100vh] flex-col items-center justify-center bg-[#282c34] text-[29px] text-white">
+      <Wrapper className="flex flex-col gap-2">
         {phrases.data?.data?.map((p, i) => <div key={i}>{p.content}</div>)}
-      </div>
-    </div>
+      </Wrapper>
+    </Container>
   );
 }
 
--- frontend/src/Components/Phrase/Phrase.tsx
+++ frontend/src/Components/Phrase/Phrase.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import { Phrase } from '../../hooks/types';
+import Container from '../Layout/Container';
 
 function PhraseItem(props: Phrase) {
   const { content } = props;
 
-  return <div>{content}</div>;
+  return <Container>{content}</Container>;
 }
 
 export default React.memo(PhraseItem);
